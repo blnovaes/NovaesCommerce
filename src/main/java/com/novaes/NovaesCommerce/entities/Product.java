3d0b91f75d2d6f457a05b94e4d1d5105d28aee89
Bruno Labate Novaes <blnovaes@hotmail.com>
2025-08-29 22:37:01 -0300

    Mapeamento Muito para muitos

--- src/main/java/com/novaes/NovaesCommerce/entities/Product.java
+++ src/main/java/com/novaes/NovaesCommerce/entities/Product.java
@@ -12,6 +12,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import java.util.HashSet;
 import java.util.Set;
@@ -40,7 +41,9 @@ public class Product {
     inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
     
-
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
+    
     public Product() {
     }
 
