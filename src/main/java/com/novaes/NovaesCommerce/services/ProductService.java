661bab5a6d2be674cfa5a93e9dbff900c1d1a365
Bruno Labate Novaes <blnovaes@hotmail.com>
2025-09-01 14:47:26 -0300

    Busca paginada

--- src/main/java/com/novaes/NovaesCommerce/services/ProductService.java
+++ src/main/java/com/novaes/NovaesCommerce/services/ProductService.java
@@ -7,8 +7,12 @@ package com.novaes.NovaesCommerce.services;
 import com.novaes.NovaesCommerce.dto.ProductDTO;
 import com.novaes.NovaesCommerce.entities.Product;
 import com.novaes.NovaesCommerce.repositories.ProductRepository;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -34,5 +38,11 @@ public class ProductService {
         ProductDTO dto = new ProductDTO(product);
         return dto;
     }
+ 
+        @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable){
+        Page<Product> result = repository.findAll(pageable);
+        return result.map(x -> new ProductDTO(x));
+    } 
     
 }
